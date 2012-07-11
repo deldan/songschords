42f47eb7c0591c5c652aaecd4c4fabecfb613261
kike <kikemetal@gmail.com>
2012-07-11 10:38:17 +0200

    Ops

--- app/Config/database.php
+++ app/Config/database.php
@@ -64,7 +64,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'BTbJwq',
+		'password' => 'root',
 		'database' => 'songs',
 		'prefix' => '',
 		'encoding' => 'utf8',
