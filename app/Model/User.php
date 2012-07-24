4dbf3c0423da81552492e3931df6895ee0c24b70
kikegarcia <kikemetal@gmail.com>
2012-07-25 00:52:18 +0200

    Para Php 5.4

--- app/Model/User.php
+++ app/Model/User.php
@@ -120,7 +120,7 @@ class User extends AppModel {
 		)
 	);
 
-	public function beforeSave() {
+	public function beforeSave($options = array()) {
 	    if(isset($this->data['User']['password'])){
 	       $this->data['User']['password'] = AuthComponent::password($this->data['User']['password']);
 	    }
