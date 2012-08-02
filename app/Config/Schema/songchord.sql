e968008caade9e4dfe0f1909aed3c9b87a7dc3d4
del_dan <aliendan@gmail.com>
2012-08-02 14:42:22 +0200

    BD

--- app/Config/Schema/songchord.sql
+++ app/Config/Schema/songchord.sql
@@ -146,3 +146,14 @@ CREATE TABLE IF NOT EXISTS `comments` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
+--
+-- Estructura de tabla para la tabla `users_songs`
+--
+
+CREATE TABLE `users_songs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `song_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
